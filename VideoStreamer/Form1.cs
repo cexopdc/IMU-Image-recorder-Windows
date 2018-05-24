c3a83b4ae7dfb5c73718db151897199d9b5d375e
Hong Xiong <ehx001@motorola.com>
2018-05-24 09:04:15 -0400

    Add project files.

---
+++ VideoStreamer/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace VideoStreamer
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo device in videoDevices)
+            {
+                comboBox1.Items.Add(device.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+
+            videoSource = new VideoCaptureDevice();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (videoSource.IsRunning == true)
+            {
+                videoSource.Stop();
+                pictureBox1.Image = null;
+                pictureBox1.Invalidate();
+            }
+            else
+            {
+                videoSource = new VideoCaptureDevice(videoDevices[comboBox1.SelectedIndex].MonikerString);
+
+                try
+                {
+                    //Check if the video device provides a list of supported resolutions
+                    if (videoSource.VideoCapabilities.Length > 0)
+                    {
+                        string highestSolution = "0;0";
+                        //Search for the highest resolution
+                        for (int i = 0; i < videoSource.VideoCapabilities.Length; i++)
+                        {
+                            if (videoSource.VideoCapabilities[i].FrameSize.Width > Convert.ToInt32(highestSolution.Split(';')[0]))
+                                highestSolution = videoSource.VideoCapabilities[i].FrameSize.Width.ToString() + ";" + i.ToString();
+                        }
+                        //Set the highest resolution as active
+                        videoSource.VideoResolution = videoSource.VideoCapabilities[Convert.ToInt32(highestSolution.Split(';')[1])];
+                    }
+                }
+                catch { }
+
+                // set NewFrame event handler
+                videoSource.NewFrame += new NewFrameEventHandler(VideoSource_NewFrame);
+                videoSource.Start();
+            }
+        }
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = image;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoSource.IsRunning)
+            {
+                videoSource.Stop();
+            }
+        }
+    }
+}
